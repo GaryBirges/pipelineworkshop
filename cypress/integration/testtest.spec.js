a51e999996c9ad796e61fb13fc2e0df3c02a2e06
GaryBirges <gary.birges@m3marketaccess.com>
2019-11-13 20:05:34 +0000

    tests added

---
+++ cypress/integration/testtest.spec.js
@@ -0,0 +1,10 @@
+import { createYield } from "typescript"
+
+context("QuickStart", ()=>{
+    it('open the quick start', ()=>{
+        cy.visit('/');
+        cy.get('.toolbar').contains("Welcome");
+        cy.get('.content').contains("app is running");
+        cy.get('.content pre').contains("ng generate component");
+    })
+})
